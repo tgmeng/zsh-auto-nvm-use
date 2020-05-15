a264de5ac9e383233d29c8cfe69103c7ce5e8126
tgmeng <tgmeng@163.com>
2020-05-15 15:16:12 +0800

    optimize use

--- zsh-auto-nvm-use.zsh
+++ zsh-auto-nvm-use.zsh
@@ -5,7 +5,13 @@ load-nvmrc() {
   local nvmrc_path="$(nvm_find_nvmrc)"
 
   if [[ -n "$nvmrc_path" ]]; then
-    nvm use
+    local nvmrc_node_version="$(cat "${nvmrc_path}")"
+
+    if [[ "$(nvm version "$nvmrc_node_version")" = "N/A" ]]; then
+      echo "version ${nvmrc_node_version} not installed"
+    elif [[ "$nvmrc_node_version" != "$node_version" ]]; then
+      nvm use
+    fi
   elif [[ "$node_version" != "$(nvm version default)" ]]; then
     echo "Reverting to nvm default version"
     nvm use default
